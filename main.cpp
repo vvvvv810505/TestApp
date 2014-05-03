c4e4c49902f386295703fc46abe4f56b08b139c2
vvvvv810505 <vvvvv810505@sina.com>
2014-05-03 17:25:35 +0800

    Test 1
    Just Test

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  TestApp
+//
+//  Created by wzhao on 5/3/14.
+//  Copyright (c) 2014 wzhao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
